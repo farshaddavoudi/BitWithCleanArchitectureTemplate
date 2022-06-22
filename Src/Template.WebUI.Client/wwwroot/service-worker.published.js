75ca107b7741d2ee3129146ba8e0712c5ff3e9b0
Farshad Davoudi <f.davoudi.r@outlook.com>
2022-06-22 15:37:03 +0430

    Add Service.Worker

---
+++ Src/Template.WebUI.Client/wwwroot/service-worker.published.js
@@ -0,0 +1,16 @@
+﻿self.assetsInclude = [];
+self.assetsExclude = [/\.scp\.css$/];
+self.defaultUrl = '/';
+self.prohibitedUrls = [];
+self.assetsUrl = '/service-worker-assets.js';
+
+// more about SRI (Subresource Integrity) here: https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
+// online tool to generate integrity hash: https://www.srihash.org/   or   https://laysent.github.io/sri-hash-generator/
+// using only js to generate hash: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+self.externalAssets = [
+    {
+        "url": "/"
+    },
+];
+
+self.importScripts('_content/Bit.Tooling.Bswup/bit-bswup.sw.js');
