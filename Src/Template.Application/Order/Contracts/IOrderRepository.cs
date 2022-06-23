488ff37ce317cc4d10bf16b8fbdde7229c0c7a1d
Farshad Davoudi <f.davoudi.r@outlook.com>
2022-06-23 15:35:43 +0430

    Remove extra Classes related to Repository Pattern

--- Src/Template.Application/Order/Contracts/IOrderRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using ATABit.Model.Data.Contracts;
-using Template.Domain.Entities.Order;
-
-namespace Template.Application.Order.Contracts;
-
-public interface IOrderRepository : IATARepository<OrderEntity>
-{
-}
--- Src/Template.Infrastructure/Persistence/EF/Repository/Order/OrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Template.Application.Order.Contracts;
-using Template.Domain.Entities.Order;
-
-namespace Template.Infrastructure.Persistence.EF.Repository.Order;
-
-public class OrderRepository : ATARepository<OrderEntity>, IOrderRepository
-{
-
-}
